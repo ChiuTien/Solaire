6b6f2c6297f433132392f212ba75200d4388bce4
ChiuTien <chiumandresychristian@gmail.com>
2026-04-17 13:05:13 +0300

    presque fini

--- Sql/DonneeAlea.sql
+++ Sql/DonneeAlea.sql
@@ -15,44 +15,50 @@ INSERT INTO Solaris.dbo.Materiel (nom) VALUES
 	 (N'Routeur wifi');
 
 
-INSERT INTO Solaris.dbo.Ressource (nom,puissanceTheorique,puissanceReelle) VALUES
-	 (N'panneaux scolaire',NULL,NULL),
-	 (N'batterie',NULL,NULL),
-	 (N'Panneau solaire scolaire',931.25,372.50),
-	 (N'Batterie scolaire',2205.00,1470.00);
+-- RESSOURCES = PANNEAUX SOLAIRES SEULEMENT
+INSERT INTO Solaris.dbo.Ressource (nom, puissanceTheorique, puissanceReelle) VALUES
+	 (N'Panneau solaire scolaire', 931.25, 372.50),
+	 (N'Panneau solaire secondaire', 500.00, 200.00);
 
 
-
-INSERT INTO Solaris.dbo.Consommation (idMateriel,puissance,heureDebut,heureFin) VALUES
-	 (1,120,'06:00:00.0000000','17:00:00.0000000'),
-	 (2,55,'08:00:00.0000000','12:00:00.0000000'),
-	 (3,75,'10:00:00.0000000','14:00:00.0000000'),
-	 (4,10,'17:00:00.0000000','19:00:00.0000000'),
-	 (2,55,'17:00:00.0000000','19:00:00.0000000'),
-	 (5,10,'19:00:00.0000000','06:00:00.0000000'),
-	 (1,120,'19:00:00.0000000','06:00:00.0000000'),
-	 (4,10,'19:00:00.0000000','23:00:00.0000000');
-
-INSERT INTO Solaris.dbo.ConfigJournee (heureDebut,heureFin,rendement,idStatut) VALUES
-	 ('06:00:00.0000000','17:00:00.0000000',40,1),
-	 ('17:00:00.0000000','19:00:00.0000000',50,2),
-	 ('19:00:00.0000000','06:00:00.0000000',150,3);
+-- BATTERIES = TABLE SEPAREE
+INSERT INTO Solaris.dbo.Batterie (capaciteTheorique, capaciteReelle, rendement) VALUES
+	 (2205.00, 1470.00, 95.0),
+	 (3440.00, 2580.00, 90.0);
 
 
-INSERT INTO Solaris.dbo.Resultat (idConfigJournee,idRessource) VALUES
-	 (1,3),
-	 (1,4),
-	 (1,3),
-	 (2,4),
-	 (1,3),
-	 (2,4),
-	 (1,3),
-	 (2,4);    
+INSERT INTO Solaris.dbo.Consommation (idMateriel, puissance, heureDebut, heureFin) VALUES
+	 (1, 120, '06:00:00', '17:00:00'),
+	 (2, 55, '08:00:00', '12:00:00'),
+	 (3, 75, '10:00:00', '14:00:00'),
+	 (4, 10, '17:00:00', '19:00:00'),
+	 (2, 55, '17:00:00', '19:00:00'),
+	 (5, 10, '19:00:00', '06:00:00'),
+	 (1, 120, '19:00:00', '06:00:00'),
+	 (4, 10, '19:00:00', '23:00:00');
+
+INSERT INTO Solaris.dbo.ConfigJournee (heureDebut, heureFin, rendement, idStatut) VALUES
+	 ('06:00:00', '17:00:00', 40, 1),
+	 ('17:00:00', '19:00:00', 30, 2),
+	 ('19:00:00', '06:00:00', 0, 3);
 
 
-INSERT INTO Solaris.dbo.ChargeBatterie (heureDebut,heureFin,Capacite,PuisanceNecessaire) VALUES
-	 ('06:00:00.0000000','17:00:00.0000000',3440.0,312.72727272727275),
-	 ('06:00:00.0000000','17:00:00.0000000',3440.0,312.72727272727275),
-	 ('06:00:00.0000000','19:00:00.0000000',3440.0,264.61538461538464),
-	 ('06:00:00.0000000','19:00:00.0000000',3440.0,264.61538461538464),
-	 ('06:00:00.0000000','19:00:00.0000000',1470.0,122.5);
+-- RESULTATS = Association Config + Ressources (panneaux)
+INSERT INTO Solaris.dbo.Resultat (idConfigJournee, idRessource) VALUES
+	 (1, 1),
+	 (1, 1),
+	 (2, 1),
+	 (2, 2),
+	 (1, 1),
+	 (2, 2),
+	 (1, 1),
+	 (2, 2);    
+
+
+-- CHARGE BATTERIE = Heures de charge de la batterie
+INSERT INTO Solaris.dbo.ChargeBatterie (heureDebut, heureFin, Capacite, PuisanceNecessaire) VALUES
+	 ('10:00:00', '14:00:00', 2205.0, 551.25),
+	 ('10:00:00', '16:00:00', 3440.0, 860.00),
+	 ('10:00:00', '15:00:00', 3440.0, 688.00),
+	 ('11:00:00', '15:00:00', 2205.0, 551.25),
+	 ('10:00:00', '17:00:00', 1470.0, 210.00);
